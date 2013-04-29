f10306613dbd1d68801208747744bbd2e8bbdcaa
Clément SIPIETER <clement@6pi.fr>
2013-04-29 23:56:13 +0200

    first commit

---
+++ .keysnail.js
@@ -0,0 +1,310 @@
+// ========================== KeySnail Init File =========================== //
+
+// You can preserve your code in this area when generating the init file using GUI.
+// Put all your code except special key, set*key, hook, blacklist.
+// ========================================================================= //
+//{{%PRESERVE%
+// Put your codes here
+//}}%PRESERVE%
+// ========================================================================= //
+
+// ========================= Special key settings ========================== //
+
+key.quitKey              = "C-g";
+key.helpKey              = "<f1>";
+key.escapeKey            = "C-q";
+key.macroStartKey        = "<f3>";
+key.macroEndKey          = "<f4>";
+key.universalArgumentKey = "C-u";
+key.negativeArgument1Key = "C--";
+key.negativeArgument2Key = "C-M--";
+key.negativeArgument3Key = "M--";
+key.suspendKey           = "<f2>";
+
+// ================================= Hooks ================================= //
+
+
+hook.setHook('KeyBoardQuit', function (aEvent) {
+    if (key.currentKeySequence.length) {
+        return;
+    }
+    command.closeFindBar();
+    var marked = command.marked(aEvent);
+    if (util.isCaretEnabled()) {
+        if (marked) {
+            command.resetMark(aEvent);
+        } else {
+            if ("blur" in aEvent.target) {
+                aEvent.target.blur();
+            }
+            gBrowser.focus();
+            _content.focus();
+        }
+    } else {
+        goDoCommand("cmd_selectNone");
+    }
+    if (KeySnail.windowType === "navigator:browser" && !marked) {
+        key.generateKey(aEvent.originalTarget, KeyEvent.DOM_VK_ESCAPE, true);
+    }
+});
+
+
+// ============================= Key bindings ============================== //
+
+key.setGlobalKey('C-M-r', function (ev) {
+    userscript.reload();
+}, 'Reload the initialization file', true);
+
+key.setGlobalKey('M-x', function (ev, arg) {
+    ext.select(arg, ev);
+}, 'List exts and execute selected one', true);
+
+key.setGlobalKey('M-:', function (ev) {
+    command.interpreter();
+}, 'Command interpreter', true);
+
+key.setGlobalKey(['<f1>', 'b'], function (ev) {
+    key.listKeyBindings();
+}, 'List all keybindings');
+
+key.setGlobalKey(['<f1>', 'F'], function (ev) {
+    openHelpLink("firefox-help");
+}, 'Display Firefox help');
+
+key.setGlobalKey('C-m', function (ev) {
+    key.generateKey(ev.originalTarget, KeyEvent.DOM_VK_RETURN, true);
+}, 'Generate the return key code');
+
+key.setGlobalKey(['C-x', 'C-f'], function (ev) {
+    command.focusToById("urlbar");
+}, 'Focus to the location bar', true);
+
+key.setGlobalKey(['C-x', 'f'], function (ev) {
+    command.focusToById("searchbar");
+}, 'Focus to the search bar', true);
+
+key.setGlobalKey(['C-x', 'C-n'], function (ev) {
+    OpenBrowserWindow();
+    command.focusToById("urlbar");
+}, 'Open new window');
+
+key.setGlobalKey(['C-x', 'C-t'], function (ev) {
+    BrowserOpenTab();
+}, 'Open the new tab');
+
+key.setGlobalKey(['C-x', 'C-k'], function (ev) {
+    BrowserCloseTabOrWindow();
+}, 'Close tab / window');
+
+key.setGlobalKey(['C-x', 'C-K'], function (ev) {
+    closeWindow(true);
+}, 'Close the window');
+
+key.setGlobalKey(['C-x', 'C-c'], function (ev) {
+    goQuitApplication();
+}, 'Exit Firefox', true);
+
+key.setGlobalKey(['C-x', 'o'], function (ev, arg) {
+    command.focusOtherFrame(arg);
+}, 'Select next frame');
+
+key.setGlobalKey(['C-x', '1'], function (ev) {
+    window.loadURI(ev.target.ownerDocument.location.href);
+}, 'Show current frame only', true);
+
+key.setGlobalKey(['C-x', 'C-s'], function (ev) {
+    saveDocument(window.content.document);
+}, 'Save current page to the file', true);
+
+key.setGlobalKey('M-w', function (ev) {
+    command.copyRegion(ev);
+}, 'Copy selected text', true);
+
+key.setGlobalKey('C-s', function (ev) {
+    command.iSearchForwardKs(ev);
+}, 'Emacs like incremental search forward', true);
+
+key.setGlobalKey('C-r', function (ev) {
+    command.iSearchBackwardKs(ev);
+}, 'Emacs like incremental search backward', true);
+
+key.setGlobalKey('C-z', function (ev) {
+    undoCloseTab();
+}, 'Undo closed tab');
+
+key.setGlobalKey('C-M-l', function (ev) {
+    getBrowser().mTabContainer.advanceSelectedTab(1, true);
+}, 'Select next tab');
+
+key.setGlobalKey('C-M-h', function (ev) {
+    getBrowser().mTabContainer.advanceSelectedTab(-1, true);
+}, 'Select previous tab');
+
+key.setEditKey(['C-x', 'h'], function (ev) {
+    command.selectAll(ev);
+}, 'Select whole text', true);
+
+key.setEditKey(['C-x', 'r', 'd'], function (ev, arg) {
+    command.replaceRectangle(ev.originalTarget, "", false, !arg);
+}, 'Delete text in the region-rectangle', true);
+
+key.setEditKey(['C-x', 'r', 't'], function (ev) {
+    prompt.read("String rectangle: ", function (aStr, aInput) {command.replaceRectangle(aInput, aStr);}, ev.originalTarget);
+}, 'Replace text in the region-rectangle with user inputted string', true);
+
+key.setEditKey(['C-x', 'r', 'o'], function (ev) {
+    command.openRectangle(ev.originalTarget);
+}, 'Blank out the region-rectangle, shifting text right', true);
+
+key.setEditKey(['C-x', 'r', 'k'], function (ev, arg) {
+    command.kill.buffer = command.killRectangle(ev.originalTarget, !arg);
+}, 'Delete the region-rectangle and save it as the last killed one', true);
+
+key.setEditKey(['C-x', 'r', 'y'], function (ev) {
+    command.yankRectangle(ev.originalTarget, command.kill.buffer);
+}, 'Yank the last killed rectangle with upper left corner at point', true);
+
+key.setEditKey('C-z', function (ev) {
+    display.echoStatusBar("Undo!", 2000);
+    goDoCommand("cmd_undo");
+}, 'Undo');
+
+key.setEditKey('C-SPC', function (ev) {
+    command.setMark(ev);
+}, 'Set the mark', true);
+
+key.setEditKey('C-o', function (ev) {
+    command.openLine(ev);
+}, 'Open line');
+
+key.setEditKey('M-z', function (ev) {
+    display.echoStatusBar("Redo!", 2000);
+    goDoCommand("cmd_redo");
+}, 'Redo');
+
+key.setEditKey(['C-l', 'C-c'], function (ev) {
+    command.beginLine(ev);
+}, 'Beginning of the line');
+
+key.setEditKey(['C-l', 'C-r'], function (ev) {
+    command.endLine(ev);
+}, 'End of the line');
+
+key.setEditKey('C-r', function (ev) {
+    command.nextChar(ev);
+}, 'Forward char');
+
+key.setEditKey('C-c', function (ev) {
+    command.previousChar(ev);
+}, 'Backward char');
+
+key.setEditKey('M-r', function (ev) {
+    command.forwardWord(ev);
+}, 'Next word');
+
+key.setEditKey('M-c', function (ev, arg) {
+    command.wordCommand(ev, arg, command.capitalizeForwardWord, command.capitalizeBackwardWord);
+}, 'Capitalize the following word');
+
+key.setEditKey('C-t', function (ev) {
+    command.nextLine(ev);
+}, 'Next line');
+
+key.setEditKey('C-s', function (ev) {
+    command.previousLine(ev);
+}, 'Previous line');
+
+key.setEditKey('C-v', function (ev) {
+    goDoCommand("cmd_deleteCharBackward");
+}, 'Delete backward char');
+
+key.setEditKey('M-v', function (ev) {
+    command.deleteBackwardWord(ev);
+}, 'Delete backward word');
+
+key.setEditKey('M-<', function (ev) {
+    command.moveTop(ev);
+}, 'Beginning of the text area');
+
+key.setEditKey('M->', function (ev) {
+    command.moveBottom(ev);
+}, 'End of the text area');
+
+key.setEditKey('C-d', function (ev) {
+    goDoCommand("cmd_deleteCharForward");
+}, 'Delete forward char');
+
+key.setEditKey('M-d', function (ev) {
+    command.deleteForwardWord(ev);
+}, 'Delete forward word');
+
+key.setEditKey('M-u', function (ev, arg) {
+    command.wordCommand(ev, arg, command.upcaseForwardWord, command.upcaseBackwardWord);
+}, 'Convert following word to upper case');
+
+key.setEditKey('M-l', function (ev, arg) {
+    command.wordCommand(ev, arg, command.downcaseForwardWord, command.downcaseBackwardWord);
+}, 'Convert following word to lower case');
+
+key.setEditKey('C-k', function (ev) {
+    command.killLine(ev);
+}, 'Kill the rest of the line');
+
+key.setEditKey('C-y', command.yank, 'Paste (Yank)');
+
+key.setEditKey('M-y', command.yankPop, 'Paste pop (Yank pop)', true);
+
+key.setEditKey('C-M-y', function (ev) {
+    if (!command.kill.ring.length) {
+        return;
+    }
+    let (ct = command.getClipboardText()) (!command.kill.ring.length || ct != command.kill.ring[0]) &&
+        command.pushKillRing(ct);
+    prompt.selector({message: "Paste:", collection: command.kill.ring, callback: function (i) {if (i >= 0) {key.insertText(command.kill.ring[i]);}}});
+}, 'Show kill-ring and select text to paste', true);
+
+key.setEditKey('C-w', function (ev) {
+    goDoCommand("cmd_copy");
+    goDoCommand("cmd_delete");
+    command.resetMark(ev);
+}, 'Cut current region', true);
+
+key.setEditKey('M-n', function (ev) {
+    command.walkInputElement(command.elementsRetrieverTextarea, true, true);
+}, 'Focus to the next text area');
+
+key.setEditKey('M-p', function (ev) {
+    command.walkInputElement(command.elementsRetrieverTextarea, false, true);
+}, 'Focus to the previous text area');
+
+key.setCaretKey('M-p', function (ev) {
+    command.walkInputElement(command.elementsRetrieverButton, true, true);
+}, 'Focus to the next button');
+
+key.setCaretKey('M-n', function (ev) {
+    command.walkInputElement(command.elementsRetrieverButton, false, true);
+}, 'Focus to the previous button');
+
+key.setCaretKey('C-t', function (ev) {
+    util.getSelectionController().scrollLine(true);
+}, 'Scroll line down');
+
+key.setCaretKey('C-s', function (ev) {
+    util.getSelectionController().scrollLine(false);
+}, 'Scroll line up');
+
+key.setCaretKey('C-c', function (ev) {
+    BrowserBack();
+}, 'backward to history');
+
+key.setCaretKey('C-r', function (ev) {
+    BrowserForward();
+}, 'forward to history');
+
+key.setGlobalKey('C-e', function (ev, arg) {
+    ext.exec('hok-start-foreground-mode', arg, ev);
+}, 'Start Hit a Hint foreground mode', true);
+
+key.setGlobalKey('C-E', function (ev, arg) {
+    ext.exec('hok-start-background-mode', arg, ev);
+}, 'Start Hit a Hint background mode', true);
---
+++ .vimrc
@@ -0,0 +1,14 @@
+" [HJKL] -> {CTSR}
+" ————————————————
+" {cr} = « gauche / droite »
+noremap c h
+noremap r l
+" {ts} = « haut / bas »
+noremap t j
+noremap s k
+" {CR} = « haut / bas de l'écran »
+noremap C H
+noremap R L
+" {TS} = « joindre / aide »
+noremap T J
+noremap S K
